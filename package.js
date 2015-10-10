70d55eb6101b6d612647c4058ef9a511db370c2d
Christie Ewen <christieewen@users.noreply.github.com>
2015-10-10 14:40:46 -0400

    Create package.js
    Initial package description and dependencies based on https://github.com/meteor/meteor/blob/devel/packages/accounts-twitter/package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  summary: "Fabric Sign in service with phone numbers",
+  version: "0.0.1"
+});
+
+Package.onUse(function(api) {
+  api.use('underscore', ['server']);
+  api.use('accounts-base', ['client', 'server']);
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', ['client', 'server']);
+  api.use('accounts-oauth', ['client', 'server']);
+  api.use('twitter', ['client', 'server']);
+
+  api.use('http', ['client', 'server']);
+
+  api.addFiles('twitter_login_button.css', 'client');
+
+  api.addFiles("twitter.js");
+});
